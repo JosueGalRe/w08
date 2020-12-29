09603abdadaa1ca471dbb81dc45e6c1c2daf36a8
JosueGalRe <josuegalre@gmail.com>
2020-12-29 11:08:17 -0600

    ✨ Added API_AUTH method

--- src/utils/API.ts
+++ src/utils/API.ts
@@ -1,5 +1,15 @@
 import axios from 'axios';
+import MD5 from 'crypto-js/md5';
 
 const API = axios.create({ baseURL: process.env.REACT_APP_END_POINT });
 
-export { API };
+const API_AUTH = (): string => {
+  const PRIVATE_KEY = process.env.REACT_APP_PRIVATE_KEY as string;
+  const PUBLIC_KEY = process.env.REACT_APP_PUBLIC_KEY as string;
+  const TIME_STAMP = Date.now();
+  const MD5_STRING = MD5(TIME_STAMP + PRIVATE_KEY + PUBLIC_KEY).toString();
+
+  return `?ts=${TIME_STAMP}&apikey=${PUBLIC_KEY}&hash=${MD5_STRING}`;
+};
+
+export { API, API_AUTH };
