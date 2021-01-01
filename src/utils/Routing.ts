df690c5a5be8855d105510f1bea86f2771f9b64e
JosueGalRe <josuegalre@gmail.com>
2021-01-01 17:04:06 -0600

    ✨ Added Routing util

---
+++ src/utils/Routing.ts
@@ -0,0 +1,33 @@
+enum ROUTE {
+  HOME = '/',
+  COMICS = '/comics/:page',
+  COMIC = '/comic/:id',
+  CHARACTERS = '/characters/:page',
+  CHARACTER = '/character/:id',
+  STORIES = '/stories/:page',
+  STORIE = '/storie/:id',
+  ERROR = '*',
+}
+
+type TArgs =
+  | { path: ROUTE.HOME }
+  | { path: ROUTE.COMICS; params: { page: number | string } }
+  | { path: ROUTE.COMIC; params: { id: number | string } }
+  | { path: ROUTE.CHARACTERS; params: { page: number | string } }
+  | { path: ROUTE.CHARACTER; params: { id: number | string } }
+  | { path: ROUTE.STORIES; params: { page: number | string } }
+  | { path: ROUTE.STORIE; params: { id: number | string } }
+  | { path: ROUTE.ERROR };
+
+type TArgsWithParams = Extract<TArgs, { path: any; params: any }>;
+
+const createPath = (args: TArgs) => {
+  if (Object.prototype.hasOwnProperty.call(args, 'params') === false) return args.path;
+
+  return Object.entries((args as TArgsWithParams).params).reduce(
+    (previousValue: string, [param, value]) => previousValue.replace(`:${param}`, `${value}`),
+    args.path
+  );
+};
+
+export { createPath, ROUTE };
