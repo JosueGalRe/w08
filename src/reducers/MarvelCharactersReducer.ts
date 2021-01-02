5e97e597b660eb96195c1646d6c7b3aec96406e3
JosueGalRe <josuegalre@gmail.com>
2021-01-02 00:04:17 -0600

    ✨ Added MarvelCharacters Reducer

---
+++ src/reducers/MarvelCharactersReducer.ts
@@ -0,0 +1,45 @@
+import {
+  IMarvelCharacters,
+  MarvelCharactersActions,
+  MarvelCharactersActionList,
+} from 'interfaces/reducers/MarvelCharactersInterfaces';
+
+const MarvelCharactersInitialState: IMarvelCharacters = {
+  data: [],
+  selected: null,
+  favorites: [],
+  disliked: [],
+};
+const MarvelCharactersReducer = (state: IMarvelCharacters = MarvelCharactersInitialState, action: MarvelCharactersActions) => {
+  switch (action.type) {
+    case MarvelCharactersActionList.SET_CHARACTERS: {
+      return { ...state, data: action.payload.data };
+    }
+    case MarvelCharactersActionList.SET_SELECTED_CHARACTER: {
+      return { ...state, selected: action.payload.data };
+    }
+    case MarvelCharactersActionList.ADD_FAVORITES_CHARACTERS: {
+      return { ...state, favorites: [...state.favorites, action.payload.id] };
+    }
+    case MarvelCharactersActionList.DELETE_FAVORITES_CHARACTERS: {
+      return { ...state, favorites: state.favorites.map((favorite) => favorite !== action.payload.id) };
+    }
+    case MarvelCharactersActionList.DELETE_ALL_FAVORITES_CHARACTERS: {
+      return { ...state, favorites: [] };
+    }
+    case MarvelCharactersActionList.ADD_DISLIKED_CHARACTERS: {
+      return { ...state, disliked: [...state.disliked, action.payload.id] };
+    }
+    case MarvelCharactersActionList.DELETE_DISLIKED_CHARACTERS: {
+      return { ...state, disliked: state.disliked.map((disliked) => disliked !== action.payload.id) };
+    }
+    case MarvelCharactersActionList.DELETE_ALL_DISLIKED_CHARACTERS: {
+      return { ...state, disliked: [] };
+    }
+    default: {
+      return state;
+    }
+  }
+};
+
+export { MarvelCharactersReducer, MarvelCharactersInitialState };
