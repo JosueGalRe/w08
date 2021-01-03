ea02607b165d41b405e829571a86229db31e3f46
JosueGalRe <josuegalre@gmail.com>
2021-01-02 23:20:11 -0600

    ✨ Added HeaderRoute Component

---
+++ src/components/HeaderRoute/index.tsx
@@ -0,0 +1,26 @@
+/* eslint-disable react/jsx-props-no-spreading */
+
+import React from 'react';
+import { Route } from 'react-router-dom';
+import Container from 'components/Container';
+import Header from 'components/Header';
+import Footer from 'components/Footer';
+
+const HeaderRoute = ({ component: Component, ...rest }: any) => {
+  return (
+    <>
+      <Header />
+      <Container>
+        <Route
+          {...rest}
+          render={() => {
+            <Component />;
+          }}
+        />
+      </Container>
+      <Footer />
+    </>
+  );
+};
+
+export default HeaderRoute;
