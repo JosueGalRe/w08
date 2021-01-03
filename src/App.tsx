a0540b7a672d4ad9889c8ccf37421b04b91563ac
JosueGalRe <josuegalre@gmail.com>
2021-01-02 23:32:12 -0600

    ✨ Added Lazy components and ReactRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
-function App() {
+import MarvelComicsContext from 'context/MarvelComicsContext';
+import React, { Suspense, useReducer } from 'react';
+import { Switch } from 'react-router';
+import { ROUTE } from 'utils/Routing';
+import { MarvelComicsInitialState, MarvelComicsReducer } from 'reducers/MarvelComicsReducer';
+import Loading from 'components/Loading';
+import HeaderRoute from 'components/HeaderRoute';
+
+const Home = React.lazy(() => import('pages/HomePage'));
+const List = React.lazy(() => import('pages/List'));
+
+const App = () => {
+  const [state, dispatch] = useReducer(MarvelComicsReducer, MarvelComicsInitialState);
+
   return (
-    <div className='App'>
-      <h1>Works!</h1>
-    </div>
+    <Suspense fallback={<Loading />}>
+      <Switch>
+        <HeaderRoute path={ROUTE.COMICS}>
+          <MarvelComicsContext.Provider value={{ state, dispatch }}>
+            <List />
+          </MarvelComicsContext.Provider>
+        </HeaderRoute>
+        <HeaderRoute path={ROUTE.HOME} exact>
+          <MarvelComicsContext.Provider value={{ state, dispatch }}>
+            <Home />
+          </MarvelComicsContext.Provider>
+        </HeaderRoute>
+      </Switch>
+    </Suspense>
   );
-}
+};
 
 export default App;
