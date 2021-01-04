fa4a5c1ef8d4156903d00521b0e5a9409f58e2a4
JosueGalRe <josuegalre@gmail.com>
2021-01-03 18:03:29 -0600

    ✨ Added remaining query parameters

--- src/utils/API_ENDPOINTS.ts
+++ src/utils/API_ENDPOINTS.ts
@@ -7,26 +7,67 @@ enum APIType {
   STORIES = 'stories',
 }
 
-type urlParams = {
+export interface urlParams {
+  title?: string;
+  format?: string;
   id?: number;
   offset?: number;
   limit?: number;
-};
+  dateRange?: string;
+  orderBy?: string;
+  name?: string;
+  modifiedSince?: string;
+  comics?: string;
+  series?: string;
+  stories?: string;
+}
 
 interface urlCreatorTypes {
-  (endpoint: string, params?: urlParams): string;
+  (endpoint: string, params?: urlParams, filter?: APIType): string;
 }
 
 const paramsWrapper = (params: urlParams): string => {
   const apiParams: Array<string> = [];
   let returningString = '';
 
+  if (params.title) {
+    apiParams.push(`title=${params.title}`);
+  }
+
+  if (params.name) {
+    apiParams.push(`name=${params.name}`);
+  }
+
+  if (params.format) {
+    apiParams.push(`format=${params.format}`);
+  }
+
+  if (params.dateRange) {
+    apiParams.push(`dateRange=${params.dateRange}`);
+  }
+
+  if (params.modifiedSince) {
+    apiParams.push(`modifiedSince=${params.modifiedSince}`);
+  }
+
   if (params.limit) {
     apiParams.push(`limit=${params.limit}`);
   }
 
+  if (params.orderBy) {
+    apiParams.push(`orderBy=${params.orderBy}`);
+  }
+
   if (params.offset) {
     apiParams.push(`offset=${params.offset}`);
+  }
+
+  if (params.stories) {
+    apiParams.push(`stories=${params.stories}`);
+  }
+
+  if (params.comics) {
+    apiParams.push(`comics=${params.comics}`);
   }
 
   if (apiParams.length > 0) {
@@ -36,11 +77,15 @@ const paramsWrapper = (params: urlParams): string => {
   return returningString;
 };
 
-const urlCreator: urlCreatorTypes = (endpoint, params) => {
+const urlCreator: urlCreatorTypes = (endpoint, params, filter) => {
   let returningURL: string = `${process.env.REACT_APP_API_VERSION as string}/public/${endpoint}`;
+
   if (params) {
     if (params?.id) {
       returningURL += `/${params.id}?`;
+      if (filter) {
+        returningURL += filter;
+      }
     } else {
       returningURL += `?${paramsWrapper(params)}`;
     }
@@ -54,8 +99,20 @@ export default {
   CHARACTERS(params?: urlParams) {
     return urlCreator(APIType.CHARACTERS, params);
   },
+  CHARACTER_COMICS(params?: urlParams) {
+    return urlCreator(APIType.CHARACTERS, params, APIType.COMICS);
+  },
+  CHARACTER_STORIES(params?: urlParams) {
+    return urlCreator(APIType.CHARACTERS, params, APIType.STORIES);
+  },
   COMICS(params?: urlParams) {
     return urlCreator(APIType.COMICS, params);
+  },
+  COMICS_STORIES(params?: urlParams) {
+    return urlCreator(APIType.COMICS, params, APIType.STORIES);
+  },
+  COMICS_CHARACTERS(params?: urlParams) {
+    return urlCreator(APIType.COMICS, params, APIType.CHARACTERS);
   },
   CREATORS(params?: urlParams) {
     return urlCreator(APIType.CREATORS, params);
@@ -68,5 +125,11 @@ export default {
   },
   STORIES(params?: urlParams) {
     return urlCreator(APIType.STORIES, params);
+  },
+  STORIES_CHARACTERS(params?: urlParams) {
+    return urlCreator(APIType.STORIES, params, APIType.CHARACTERS);
+  },
+  STORIES_COMICS(params?: urlParams) {
+    return urlCreator(APIType.STORIES, params, APIType.COMICS);
   },
 };
