12aa705466ec7fa839a78febc6e48038383137bc
JosueGalRe <josuegalre@gmail.com>
2021-01-03 18:08:35 -0600

    💄 Changed MainContent background Image

--- src/components/MainContent/index.tsx
+++ src/components/MainContent/index.tsx
@@ -1,11 +1,11 @@
-import './MainContent.scss';
-import BackgroundImage from 'assets/marvel.svg';
 import { memo } from 'react';
+import BackgroundImage from 'assets/marvel_background.jpg';
+import './MainContent.scss';
 
 const MainContent = () => {
   return (
     <div className='main__content'>
-      <img src={BackgroundImage} alt='Main Background' className='post__image' />
+      <img src={BackgroundImage} alt='Main Background' className='background__image' />
       <div className='gradient' />
       <div className='main__content-text'>
         <p className='main__content-title'>Welcome to Marvel Comics</p>
