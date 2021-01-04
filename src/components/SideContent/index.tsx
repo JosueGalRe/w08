fb4365507272cb5edb8a045bb136bc752580592d
JosueGalRe <josuegalre@gmail.com>
2021-01-03 18:06:47 -0600

    ✨ Added Marvel Despcription

--- src/components/SideContent/index.tsx
+++ src/components/SideContent/index.tsx
@@ -1,15 +1,18 @@
-import { ReactNode } from 'react';
 import './SideContent.scss';
 
-type SideContainerTypes = {
-  children: ReactNode;
-};
-
-const SideContainer = ({ children }: SideContainerTypes) => {
+const SideContainer = () => {
   return (
     <div className='side__content'>
-      <p className='side__content-title'>Last added games</p>
-      <div className='side__content-wrapper'>{children}</div>
+      <p className='side__content-title'>About Marvel Comics</p>
+      <div className='side__content-wrapper'>
+        <p className='home__page-text'>
+          Marvel Comics is an American comic book publisher. It is popularly known as &quot;The House of Ideas&quot; for the
+          creation of numerous emblematic characters of the superhero genre. Some of its best known comics are The Fantastic 4,
+          Spider-Man, Daredevil, Captain America, The Avengers, Iron Man, Hulk, Wolverine and the X-Men. Since the 60&apos;s the
+          company has been one of the largest publishers of this genre. Located in New York, it was founded at 387 Park Avenue
+          South, with its current offices at 40th East 10th Avenue.
+        </p>
+      </div>
     </div>
   );
 };
