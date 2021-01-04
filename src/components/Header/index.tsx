f8234e80699480e2ceddb7c67366a8c203a6d5b5
JosueGalRe <josuegalre@gmail.com>
2021-01-04 15:08:47 -0600

    ✨ Added Bookmarks link

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,18 +7,23 @@ import './Header.scss';
 const Header = () => {
   return (
     <div className='header__container'>
-      <Link type='button' className='header__title' to={ROUTE.HOME}>
+      <Link type='button' className='header__image' to={ROUTE.HOME}>
         <img src={logo} alt='Marvel logo' />
       </Link>
-      <Link type='button' className='header__title' to={createPath({ path: ROUTE.CHARACTERS, params: { page: 1 } })}>
-        <p>Characters</p>
-      </Link>
-      <Link type='button' className='header__title' to={createPath({ path: ROUTE.COMICS, params: { page: 1 } })}>
-        <p>Comics</p>
-      </Link>
-      <Link type='button' className='header__title' to={createPath({ path: ROUTE.STORIES, params: { page: 1 } })}>
-        <p>Stories</p>
-      </Link>
+      <div className='header__list'>
+        <Link type='button' className='header__title' to={createPath({ path: ROUTE.CHARACTERS, params: { page: 1 } })}>
+          <p>Characters</p>
+        </Link>
+        <Link type='button' className='header__title' to={createPath({ path: ROUTE.COMICS, params: { page: 1 } })}>
+          <p>Comics</p>
+        </Link>
+        <Link type='button' className='header__title' to={createPath({ path: ROUTE.STORIES, params: { page: 1 } })}>
+          <p>Stories</p>
+        </Link>
+        <Link type='button' className='header__title' to={createPath({ path: ROUTE.BOOKMARKS })}>
+          <p>My bookmarks</p>
+        </Link>
+      </div>
     </div>
   );
 };
