b09677ec7ce82ef0e6dba6dcd20a68df14f5f75b
JosueGalRe <josuegalre@gmail.com>
2021-01-04 20:42:36 -0600

    ✨ Wrapping up the pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,60 @@
-import MarvelComicsContext from 'context/MarvelComicsContext';
 import React, { Suspense, useReducer } from 'react';
 import { Switch } from 'react-router';
 import { ROUTE } from 'utils/Routing';
-import { MarvelComicsInitialState, MarvelComicsReducer } from 'reducers/MarvelComicsReducer';
+import { MarvelContextInitialState, MarvelContextReducer } from 'reducers/MarvelContextReducer';
 import Loading from 'components/Loading';
-import HeaderRoute from 'components/HeaderRoute';
+import FullWrapperRoute from 'components/FullWrapperRoute';
+import HeaderOnlyRoute from 'components/HeaderOnlyRoute';
+import DataContext from 'context/DataContext';
 
 const Home = React.lazy(() => import('pages/HomePage'));
-const List = React.lazy(() => import('pages/List'));
+const ComicList = React.lazy(() => import('pages/ComicList'));
+const CharacterList = React.lazy(() => import('pages/CharacterList'));
+const StoryList = React.lazy(() => import('pages/StoryList'));
+const ComicPage = React.lazy(() => import('pages/ComicPage'));
+const CharacterPage = React.lazy(() => import('pages/CharacterPage'));
+const StoryPage = React.lazy(() => import('pages/StoryPage'));
+const Bookmarks = React.lazy(() => import('pages/Bookmarks'));
 
 const App = () => {
-  const [state, dispatch] = useReducer(MarvelComicsReducer, MarvelComicsInitialState);
+  const [state, dispatch] = useReducer(MarvelContextReducer, MarvelContextInitialState);
+
+  const dataContext = {
+    state,
+    dispatch,
+  };
 
   return (
-    <Suspense fallback={<Loading />}>
-      <Switch>
-        <HeaderRoute path={ROUTE.COMICS}>
-          <MarvelComicsContext.Provider value={{ state, dispatch }}>
-            <List />
-          </MarvelComicsContext.Provider>
-        </HeaderRoute>
-        <HeaderRoute path={ROUTE.HOME} exact>
-          <MarvelComicsContext.Provider value={{ state, dispatch }}>
+    <DataContext.Provider value={dataContext}>
+      <Suspense fallback={<Loading />}>
+        <Switch>
+          <HeaderOnlyRoute path={ROUTE.BOOKMARKS}>
+            <Bookmarks />
+          </HeaderOnlyRoute>
+          <FullWrapperRoute path={ROUTE.STORIE}>
+            <StoryPage />
+          </FullWrapperRoute>
+          <FullWrapperRoute path={ROUTE.CHARACTER}>
+            <CharacterPage />
+          </FullWrapperRoute>
+          <FullWrapperRoute path={ROUTE.COMIC}>
+            <ComicPage />
+          </FullWrapperRoute>
+          <FullWrapperRoute path={ROUTE.STORIES}>
+            <StoryList />
+          </FullWrapperRoute>
+          <FullWrapperRoute path={ROUTE.CHARACTERS}>
+            <CharacterList />
+          </FullWrapperRoute>
+          <FullWrapperRoute path={ROUTE.COMICS}>
+            <ComicList />
+          </FullWrapperRoute>
+          <FullWrapperRoute path={ROUTE.HOME} exact>
             <Home />
-          </MarvelComicsContext.Provider>
-        </HeaderRoute>
-      </Switch>
-    </Suspense>
+          </FullWrapperRoute>
+        </Switch>
+      </Suspense>
+    </DataContext.Provider>
   );
 };
 
